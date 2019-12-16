67aae34fcb2cebdbcc62a2c0d186e69c17e5db2b
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-16 00:58:13 -0300

    Added celsius_to_fahr

---
+++ fahr_to_celsius.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() {
+  float fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 100;
+  step = 20;
+
+  celsius = lower;
+  while (celsius <= upper) {
+    fahr = 9.0 * celsius / 5.0 + 32;
+    printf("%6.0f\t%4.1f\n", celsius, fahr);
+    celsius = celsius + step;
+  }
+}
