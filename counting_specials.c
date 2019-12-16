e961f1ba0ee85435250c852e1e913aedddcf419d
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-16 20:41:45 -0300

    Added counting_specials.c

---
+++ counting_specials.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main() {
+  int tabs = 0;
+  int blanks = 0;
+  int lines = 0;
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    switch (c) {
+      case '\t':
+        ++tabs;
+        break;
+      case ' ':
+        ++blanks;
+        break;
+      case '\n':
+        ++lines;
+        break;
+    }
+  }
+
+  printf("tabs: %d\nblanks: %d\nlines: %d\n", tabs, blanks, lines);
+}
