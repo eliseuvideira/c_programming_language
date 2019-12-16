6326bf120a9c718370a2af799e08ebbec657db82
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-16 19:36:34 -0300

    Added fahr_to_celsius_2, using a for loop

---
+++ fahr_to_celsius_2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main() {
+  int fahr;
+  printf("fahr\tcelsius\n");
+  for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
+    printf("%4d\t%7.1f\n", fahr, 5.0 / 9.0 * (fahr - 32));
+  }
+}
