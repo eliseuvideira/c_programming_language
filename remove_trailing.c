6d7412b28f3ef6ad857a21adf00a83aaa781af0b
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-17 23:53:59 -0300

    Refactored remove_trailing.c to print directly to console instead of storing it into an array

--- remove_trailing.c
+++ remove_trailing.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-#define MAX_TEXT_WIDTH 150000
 #define MAX_LINE_WIDTH 1000
 
 int get_line(char line[], int max_length);
@@ -8,21 +7,17 @@ int remove_trailing(char line[], int length);
 int append_text(char text[], int position, int max_length, char line[]);
 
 int main() {
-  char text[MAX_TEXT_WIDTH];
   char current_line[MAX_LINE_WIDTH];
   int length;
-  int text_length = 0;
 
   while ((length = get_line(current_line, MAX_LINE_WIDTH)) > 0) {
     if (length > 1) {
       length = remove_trailing(current_line, length);
       if (length > 1) {
-        text_length = append_text(text, text_length, MAX_TEXT_WIDTH, current_line);
+        printf("%s", current_line);
       }
     }
   }
-
-  printf("%s", text);
 
   return 0;
 }
