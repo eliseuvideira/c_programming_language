b646fa7782a026f4033a6049f69d99e6f13ce148
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-18 21:06:58 -0300

    Fixed bug that didn't account for strings

--- remove_comments.c
+++ remove_comments.c
@@ -6,9 +6,14 @@
 */
 #include <stdio.h>
 
+#define IN  1
+#define OUT 0
+
 void remove_comments(int c) ;
 void remove_multi_line_comment();
 void remove_single_line_comment();
+void print_quote(int c);
+void print_char(int c);
 
 int main() {
   int c;
@@ -16,11 +21,17 @@ int main() {
   while ((c = getchar()) != EOF) {
     remove_comments(c);
   }
+  putchar('"');
+  printf("This /* will not */ be removed, also not // this \"\n");
 }
 
 void remove_comments(int c) {
   int d;
-  if (c == '/') {
+  if (c == '"') {
+    print_quote(c);
+  } else if (c == '\'') {
+    print_char(c);
+  } else if (c == '/') {
     d = getchar();
     if (d == '*') {
       remove_multi_line_comment();
@@ -48,4 +59,39 @@ void remove_multi_line_comment() {
 void remove_single_line_comment() {
   int c;
   while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void print_quote(int c) {
+  int scaped = OUT;
+  putchar(c);
+  do {
+    if (c == '\\') {
+      if (scaped == OUT) {
+        scaped = IN;
+      } else {
+        scaped = OUT;
+      }
+    } else {
+      scaped = OUT;
+    }
+    putchar((c = getchar()));
+  } while (c != '"' || scaped == IN);
+}
+
+void print_char(int c) {  
+  int scaped = OUT;
+  putchar(c);
+  do {
+    if (c == '\\') {
+      if (scaped == OUT) {
+        scaped = IN;
+      } else {
+        scaped = OUT;
+      }
+    } else {
+      scaped = OUT;
+    }
+    c = getchar();
+    putchar(c);
+  } while (c != '\'' || scaped == IN);
 }
