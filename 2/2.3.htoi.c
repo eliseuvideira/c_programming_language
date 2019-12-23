f8fcee31bdedf15bd5e56d07e7237119370cb6b8
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-23 19:12:30 -0300

    Fixed htoi.c that was half done/wrong

--- 2/2.3.htoi.c
+++ 2/2.3.htoi.c
@@ -2,6 +2,7 @@
 
 int htoi(char hex[]);
 int power(int base, int exp);
+int hex_to_int(char hex);
 
 int main() {
   int c;
@@ -26,14 +27,16 @@ int htoi(char hex[]) {
   int pos = 0;
   int i;
   int length = 0;
-  int result;
+  int result = 0;
   if (hex[0] == '0' && (hex[1] == 'x' || hex[1] == 'X')) {
     pos += 2;
   }
 
-  for (length = 0; hex[length] != '\0'; ++length);
+  for (length = 0; hex[pos + length] != '\0'; ++length);
 
-  result = power(16, length - pos) - 1;
+  for (i = 0; i < length; ++i) {
+    result = result + hex_to_int(hex[pos + i]) * power(16, length - i - 1);
+  }
 
   return result;
 }
@@ -45,4 +48,16 @@ int power(int base, int exp) {
     value = value * base;
   }
   return value;
+}
+
+int hex_to_int(char hex) {
+  if (hex >= '0' && hex <= '9') {
+    return hex - '0';
+  }
+  if (hex >= 'A' && hex <= 'F') {
+    return hex - 'A' + 10;
+  }
+  if (hex >= 'a' && hex <= 'f') {
+    return hex - 'a' + 10;
+  }
 }
