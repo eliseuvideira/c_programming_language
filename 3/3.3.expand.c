9bc602df7cb21b1f54edfd026a915896e184329d
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-27 19:12:21 -0300

    Fixed error that printed non-alphanumeric ranges

--- 3/3.3.expand.c
+++ 3/3.3.expand.c
@@ -4,7 +4,7 @@
 
 void read_text(char s[]);
 void expand(char s1[], char s2[]);
-int is_alphanumeric(char c);
+int is_alphanumeric_range(char c1, char c2);
 
 int main() {
   char s[MAX_LENGTH], t[MAX_LENGTH];
@@ -31,7 +31,7 @@ void expand(char s1[], char s2[]) {
 
   s2[0] = s1[0];
   for (i = j = 1; s1[i] != '\0'; ++i) {
-    if (s1[i] == '-' && (is_alphanumeric(s1[i - 1])) && (is_alphanumeric(s1[i + 1])) && s1[i - 1] + 1 < s1[i + 1]) {
+    if (s1[i] == '-' && is_alphanumeric_range(s1[i - 1], s1[i + 1])) {
       for (c = s1[i - 1] + 1; c < s1[i + 1]; ++c, ++j) {
         s2[j] = c;
       }
@@ -43,14 +43,14 @@ void expand(char s1[], char s2[]) {
   s2[++j] = '\0';
 }
 
-int is_alphanumeric(char c) {
-  if (c >= 'a' && c <= 'z') {
+int is_alphanumeric_range(char c1, char c2) {
+  if (c1 >= 'a' && c1 <= 'z' && c2 >= 'a' && c2 <= 'z' && c2 > c1 + 1) {
     return 1;
   }
-  if (c >= 'A' && c <= 'A') {
+  if (c1 >= 'A' && c1 <= 'A' && c2 >= 'A' && c2 <= 'Z' && c2 > c1 + 1) {
     return 1;
   }
-  if (c >= '0' && c <= '9') {
+  if (c1 >= '0' && c1 <= '9' && c2 >= '0' && c2 <= '9' && c2 > c1 + 1) {
     return 1;
   }
   return 0;
