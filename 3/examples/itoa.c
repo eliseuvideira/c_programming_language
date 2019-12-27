7eeb53c57a0886e0bbd4c15dea4ec51c893b665c
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-27 20:18:28 -0300

    Added itoa.c

---
+++ 3/examples/itoa.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 65535
+
+void itoa(int n, char s[]);
+void reverse(char s[]);
+
+int main() {
+  int n = -150;
+  char s[MAX_LENGTH];
+
+  itoa(n, s);
+
+  printf("%s\n", s);
+
+  return 0;
+}
+
+void itoa(int n, char s[]) {
+  int i, sign;
+
+  if ((sign = n) < 0) {
+    n = -n;
+  }
+  i = 0;
+  do {
+    s[i++] = n % 10 + '0';
+  } while ((n /= 10) > 0);
+  if (sign < 0) {
+    s[i++] = '-';
+  }
+  s[i] = '\0';
+  reverse(s);
+}
+
+void reverse(char s[]) {
+  int c, i, j;
+
+  for (i = 0, j = strlen(s) - 1; i < j; ++i, --j) {
+    c = s[i], s[i] = s[j], s[j] = c;
+  }
+}
+
