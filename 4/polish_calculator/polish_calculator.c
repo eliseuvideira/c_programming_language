1827a67a33d5d415cfbbe5972bfb2915aa6d0c02
Eliseu Videira <eliseuvideira1@gmail.com>
2019-12-29 14:38:58 -0300

    added print, swap and duplicate to the polish_calculator

--- 4/polish_calculator/polish_calculator.c
+++ 4/polish_calculator/polish_calculator.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 #define MAXOP 100
 #define NUMBER '0'
+#define NAMED '1'
 
 int get_operator(char s[]);
 
 int main() {
   int type;
 
-  double operand1, operand2;
+  double x, y;
 
   char s[MAXOP];
 
@@ -22,35 +24,51 @@ int main() {
       case NUMBER:
         push(atof(s));
         break;
+      case NAMED:
+        if (strcmp(s, "print") == 0) {
+          x = pop();
+          printf("\t%10.8f\n", x);
+          push(x);
+        } else if (strcmp(s, "duplicate") == 0) {
+          x = pop();
+          push(x);
+          push(x);
+        } else if (strcmp(s, "swap") == 0) {
+          y = pop();
+          x = pop();
+          push(y);
+          push(x);
+        }
+        break;
       case '+':
-        operand2 = pop();
-        operand1 = pop();
-        push(operand1 + operand2);
+        y = pop();
+        x = pop();
+        push(x + y);
         break;
       case '*':
-        operand2 = pop();
-        operand1 = pop();
-        push(operand1 * operand2);
+        y = pop();
+        x = pop();
+        push(x * y);
         break;
       case '-':
-        operand2 = pop();
-        operand1 = pop();
-        push(operand1 - operand2);
+        y = pop();
+        x = pop();
+        push(x - y);
         break;
       case '/':
-        operand2 = pop();
-        operand1 = pop();
-        if (operand2 != 0.0) {
-          push(operand1 / operand2);
+        y = pop();
+        x = pop();
+        if (y != 0.0) {
+          push(x / y);
         } else {
           printf("error: division by zero\n");
         }
         break;
       case '%':
-        operand2 = pop();
-        operand1 = pop();
-        if ((int)operand2 != 0) {
-          push((int)operand1 % (int)operand2);
+        y = pop();
+        x = pop();
+        if ((int)y != 0) {
+          push((int)x % (int)y);
         } else {
           printf("error: division by zero\n");
         }
@@ -77,10 +95,22 @@ int get_operator(char s[]) {
 
   s[1] = '\0';
   i = 0;
-  if (!isdigit(c) && c != '.' && c != '-') {
+  if (!isdigit(c) && c != '.' && c != '-' && !islower(c)) {
     return c;
   }
 
+  if (islower(c)) {
+    while (islower(s[++i] = c = getch()));
+    s[i] = '\0';
+    if (c != EOF) {
+      ungetch(c);
+    }
+    if (strlen(s) > 1) {
+      return NAMED;
+    } else {
+      return c;
+    }
+  }
   if (c == '-') {
     if (isdigit(c = getch()) || c == '.') {
       s[++i] = c;
